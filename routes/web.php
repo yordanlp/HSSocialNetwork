7a958665bdf685a442ce3d1d9810903230b5a94c
Yordán Luis Laborde Peña <C.YLABOR@tessa.com.uy>
2022-11-09 18:49:19 -0500

    Routing root to Index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FeedController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FeedController::class, 'Index']);
