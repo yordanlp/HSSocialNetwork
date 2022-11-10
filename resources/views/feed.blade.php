671e090419af1822d85d2b9e8298e281622de535
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2022-11-10 16:44:59 -0500

    wip

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,5 +1,7 @@
 <x-layout>
-    <x-Post />
-    <x-Post />
-    <x-Post />
+    <div class="d-flex flex-column gap-2 mx-auto" style="width: 50%;">
+        <x-Post />
+        <x-Post />
+        <x-Post />
+    </div>
 </x-layout>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,22 @@
 <x-layout>
-    <x-Post />
+    <div class="row">
+        <div class="col-3">
+            Followers
+            <x-UserCard/>
+            <x-UserCard/>
+            <x-UserCard/>
+            <x-UserCard/>
+        </div>
+        <div class="col-6">
+            <x-Post />
+        </div>
+        <div class="col-3">
+            Following
+            <x-UserCard/>
+            <x-UserCard/>
+            <x-UserCard/>
+            <x-UserCard/>
+            <x-UserCard/>
+        </div>
+    </div>
 </x-layout>
