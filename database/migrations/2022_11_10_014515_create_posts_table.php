eb35c08cd4c1ab7e1b7009f7b60447bbc26cd179
Yordán Luis Laborde Peña <C.YLABOR@tessa.com.uy>
2022-11-10 16:49:59 -0500

    adding migration for posts

---
+++ database/migrations/2022_11_10_014515_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('photo');
+            $table->string('message');
+            $table->integer('likes')->default(0);
+            $table->integer('dislikes')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
