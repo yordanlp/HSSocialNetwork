87a5e639ecf1a4b320675bd552ab35692f65e1cf
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2022-11-10 18:12:46 -0500

    retreiving all users to show in index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function Index()
     {
-        return view("people");
+        $users = User::all();
+        return view("people", [
+            "users" => $users
+        ]);
     }
 }
