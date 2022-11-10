7b3f4d611f0da021d037f1dccd0cf1d30b5101d5
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2022-11-10 16:44:46 -0500

    created people view

---
+++ resources/views/people.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <div class="user_list">
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+        <x-UserCard/>
+    </div>
+</x-layout>
