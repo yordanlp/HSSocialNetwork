65692a3f9e0a46029197b932e6a481ae92a4c45f
Yordán Luis Laborde Peña <C.YLABOR@tessa.com.uy>
2022-11-16 14:50:02 -0500

    eager loading relationships

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,7 +9,7 @@ class ProfileController extends Controller
 {
     public function Index($user_id)
     {
-        $user = User::find($user_id);
+        $user = User::with('posts', 'posts.comments', 'posts.user')->find($user_id);
         return view("profile", [
             "user" => $user
         ]);
