141f3dbdaeae789c6e2fe0ec60e264f4e6cf5fff
Yordán Luis Laborde Peña <C.YLABOR@tessa.com.uy>
2022-11-16 14:50:52 -0500

    retreiving posts for following users and public posts

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -10,7 +10,19 @@ class FeedController extends Controller
     public function Index()
     {
         //TODO: Retrieve only friends posts and public posts
-        $posts = Post::orderBy("created_at", "desc")->get();
+        $user = auth()->user();
+        $following_users = $user->following->map(fn ($f) => $f['id'])->toArray();
+
+        $posts = Post::orderBy("created_at", "desc")
+            ->whereNull('post_id')
+            ->where(function ($query) use ($following_users) {
+                $query->whereIn('id', $following_users)
+                    ->orWhere('is_public', '=', true);
+            })
+            ->with('user')
+            ->with('comments')
+            ->get();
+
         return view("feed", [
             'posts' => $posts
         ]);
