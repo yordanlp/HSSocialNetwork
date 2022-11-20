e16f24c1b74c8006c66b397f76472a6178bb36b1
Yordán Luis Laborde Peña <C.YLABOR@tessa.com.uy>
2022-11-19 23:40:35 -0500

    adding media to posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
