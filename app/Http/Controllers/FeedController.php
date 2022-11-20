0b7103270cd41ca3cfa23013326845f36dfee0fd
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2022-11-20 05:33:04 -0500

    chanched query to retreive comments on feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -16,13 +16,9 @@ class FeedController extends Controller
         $following_users = $is_logged_in ? $user->following->map(fn ($f) => $f['id'])->toArray() : [];
 
         $posts = Post::orderBy("created_at", "desc")
-            ->whereNull('post_id')
-            ->where(function ($query) use ($following_users, $user) {
-                $query->whereIn('user_id', $following_users)
-                    ->orWhere('is_public', '=', true)
-                    ->orWhere('user_id', '=', $user->id);
-            })
-            ->with('user', 'comments', 'likes', 'dislikes', 'media')
+            ->whereIn('user_id', $following_users)
+            ->orWhere('is_public', '=', true)
+            ->with('user', 'comments', 'likes', 'dislikes', 'media', 'parent')
             ->get();
 
         return view("feed", [
