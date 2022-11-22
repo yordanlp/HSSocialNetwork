3fe06c3e5fdabea50b7640138ab214496128a634
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2022-11-21 19:11:57 -0500

    adding tailwind only in the login view

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,7 @@
 
     <!-- Scripts -->
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <!-- Custom fonts for this template-->
 
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
