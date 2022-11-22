77b17f15373a99bd621b1802ce70c78dd2bdd240
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2022-11-22 11:28:12 -0500

    adding ondelete cascade

--- database/migrations/2022_11_10_014515_create_posts_table.php
+++ database/migrations/2022_11_10_014515_create_posts_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('message')->nullable();
             $table->boolean('is_public')->default(false);
-            $table->foreignId('post_id')->nullable()->references('id')->on('posts');
+            $table->foreignId('post_id')->nullable()->references('id')->on('posts')->onDelete('cascade');
             $table->timestamps();
         });
     }
