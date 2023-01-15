a2a114f61f3aae713f0d9432587ea1cc69f711fd
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-01-15 15:38:18 +0100

    installing flysystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,7 +44,7 @@ return [
             'throw' => false,
         ],
 
-        's3' => [
+        'hssocialnetwork' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
