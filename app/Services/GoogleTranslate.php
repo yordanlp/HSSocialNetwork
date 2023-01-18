e64cce1eace00ecb463c3d6f4e5671525d38ccf2
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-01-18 21:37:56 +0100

    Creating and custom exceptions for GoogleTranslate

--- app/Services/GoogleTranslate.php
+++ app/Services/GoogleTranslate.php
@@ -2,23 +2,33 @@
 
 namespace App\Services;
 
+use App\Exceptions\NotSupportedLanguageException;
 use App\Exceptions\NullTargetLanguageException;
 use Exception;
 use Illuminate\Support\Facades\Http;
 use SebastianBergmann\CodeCoverage\Report\Xml\Source;
 
+use function PHPUnit\Framework\returnSelf;
+
 class GoogleTranslate
 {
     private string $api_key = "AIzaSyCW69t5qNg4118tniWUxCJe85yKVFISF4Q";
     private string $base_url = "https://translation.googleapis.com/language/translate/v2/";
+    private $supported_languages = null;
     public function __construct()
     {
+        $this->supported_languages = $this->get_languages();
     }
 
     public function translate(string $text, string $target, string $source = null)
     {
         if ($source == null)
             $source = $this->detect_language($text);
+
+        if (!$this->is_supported_language($source))
+            throw new NotSupportedLanguageException("Language {$source} is not supported for translation");
+        if (!$this->is_supported_language($target))
+            throw new NotSupportedLanguageException("Language {$target} is not supported for translation");
 
         if ($target == null)
             throw new NullTargetLanguageException("Target language must not be null");
@@ -30,6 +40,17 @@ class GoogleTranslate
         ]);
 
         return $data->data->translations[0]->translatedText;
+    }
+
+    public function is_supported_language(string $language)
+    {
+        return $this->supported_languages->contains($language);
+    }
+
+    public function get_languages()
+    {
+        $data = $this->get_request('languages');
+        return collect($data->data->languages)->map(fn ($lang) => $lang->language);
     }
 
     public function detect_language($text)
