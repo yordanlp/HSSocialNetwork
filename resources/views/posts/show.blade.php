0a5c0b72dd988057da8d3676783e41f42b39fe6d
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-01-22 20:21:53 +0100

    using PostsWrapper component

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,12 +1,10 @@
-<x-layout>
+<x-layout :usesLivewire="true">
     <div class="row">
         <div class="col-4 d-flex align-items-center p-4">
             <x-Post :post="$post"/>
         </div>
         <div class="col-4 overflow-auto" style="height: 80vh; display: flex; flex-direction: column; gap: 5px;">
-            @foreach ($post->comments as $comment)
-                <x-Post :post="$comment"/>
-            @endforeach
+            <livewire:posts-wrapper :section="'comments'"/>
         </div>
         <div class="col-4 d-flex align-items-center p-4">
             <x-create-post :parent_post="$post->id" action='{{route("post.store")}}' method='post' is_public='0' message='' />
